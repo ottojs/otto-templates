b975dd9e37b493a17effec24ae5803b7904d9fa5
Ryan Lelek <ryanlelek@gmail.com>
2014-07-24 15:02:14 -0700

    Rendering template directly in this module instead of otto-response

--- test/main.test.js
+++ test/main.test.js
@@ -9,14 +9,49 @@ var otto_templates = require('../lib/index.js');
 
 describe('Otto Templates', function () {
 
-  describe('.show_view()', function () {
+  describe('.render()', function () {
 
-    it('should set res.locals._view and res.locals.title', function () {
-      var res = { locals : {} };
-      otto_templates.show_view({}, res, function () {
-        res.locals.should.have.property('_view').and.equal('index');
-        res.locals.should.have.property('title').and.equal('MyCustomTitle');
+    it('should use res.render to render the template', function () {
+
+      var called = false;
+      var res = {
+        render : function () {
+          called = true;
+        }
+      };
+
+      var middleware = otto_templates.render();
+      middleware({}, res, function () {
+        called.should.equal(true);
       });
+
+    });
+
+    it('should use a custom template name when provided', function () {
+
+      var res = {
+        render : function (template, data) {
+          template.should.equal('my-custom-template');
+        }
+      };
+
+      var middleware = otto_templates.render('my-custom-template');
+      middleware({}, res, function () {});
+
+    });
+
+    it('should use res.locals for data when rendering a template', function () {
+
+      var res = {
+        locals : { key : 'value', hello : 'world' },
+        render : function (template, data) {
+          data.should.eql({ key : 'value', hello : 'world' });
+        }
+      };
+
+      var middleware = otto_templates.render('template');
+      middleware({}, res, function () {});
+
     });
 
   });
